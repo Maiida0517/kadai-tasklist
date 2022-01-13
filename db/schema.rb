77843353ab34355d7ce120cb920bd3ad9aca5ad7
mamoru.iida <avantdor.700.0vot.517@gmail.com>
2022-01-13 11:16:51 +0000

    tasklist-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_123036) do
+ActiveRecord::Schema.define(version: 2022_01_12_142543) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
